e027cb13d560eb3e4d9c9cc21e277eeccc3a603c
yeremmi789 <yeremmi789@gmail.com>
2024-06-14 12:57:07 -0600

    productos-terminado

--- src/app/reutilizables/producto-carousel/producto-carousel.component.ts
+++ src/app/reutilizables/producto-carousel/producto-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { CommonModule, NgFor, NgStyle } from '@angular/common';  // Importar CommonModule
 import { BrowserModule } from '@angular/platform-browser';
@@ -14,22 +14,177 @@ import { BrowserModule } from '@angular/platform-browser';
   templateUrl: './producto-carousel.component.html',
   styleUrl: './producto-carousel.component.css'
 })
-export class ProductoCarouselComponent {
+export class ProductoCarouselComponent implements OnInit{
 
-  // products = [
-  //   {
-  //     image: 'https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24StandardEdition_EACanada_S2_1200x1600-5ecbb77fd9c0601ef34c4a988120fc42?h=480&quality=medium&resize=1&w=360',
-  //     category: 'Juego base',
-  //     title: 'Grand Theft Auto V: Premium Edition',
-  //     discount: '-50 %',
-  //     originalPrice: '599',
-  //     discountedPrice: '219.80'
-  //   },
 
-  //   // Añadir más productos según sea necesario
-  // ];
+  ngOnInit() {
+    this.calculateCarouselWidth();
+    this.calculateCarouselWidth_v2();
+  }
+
+  calculateCarouselWidth() {
+    const productWidth = 198 + 20; // Ancho del producto + márgenes
+    this.carouselWidth = this.products.length * productWidth;
+  }
+
+  calculateCarouselWidth_v2() {
+    const productWidth = 198 + 20; // Ancho del producto + márgenes
+    this.carouselWidth_v2 = this.products_v2.length * productWidth;
+  }
+
 
   products = [
+    { name: 'EA SPORTS FC™ 24 Standard Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24StandardEdition_EACanada_S2_1200x1600-5ecbb77fd9c0601ef34c4a988120fc42?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Red Dead Redemtion 2',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/epic/offer/RDR2PC1227_Epic Games_860x1148-860x1148-b4c2210ee0c3c3b843a8de399bfe7f5c.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Grand Theft Auto V: Premium Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_1200x1600_Portrait Store Banner-1200x1600-382243057711adf80322ed2aeea42191.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Hogwars Legacy',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S2_1200x1600-2bb94423bf1c7e2fca10577d9f2878b9?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: "Assasin's Creed  Mirage",
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/9bcf5a4dc1d54cb6ab1a42f3a70c5cf4/Carousel_BoxArt_1200x1600_1200x1600-38bda67bb1290f58e8a18ddc38a3c0ec?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+
+
+    //
+
+
+
+
+
+    { name: 'EA SPORTS FC™ 24 Standard Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24StandardEdition_EACanada_S2_1200x1600-5ecbb77fd9c0601ef34c4a988120fc42?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Red Dead Redemtion 2',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/epic/offer/RDR2PC1227_Epic Games_860x1148-860x1148-b4c2210ee0c3c3b843a8de399bfe7f5c.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Grand Theft Auto V: Premium Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_1200x1600_Portrait Store Banner-1200x1600-382243057711adf80322ed2aeea42191.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Hogwars Legacy',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S2_1200x1600-2bb94423bf1c7e2fca10577d9f2878b9?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: "Assasin's Creed  Mirage",
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/9bcf5a4dc1d54cb6ab1a42f3a70c5cf4/Carousel_BoxArt_1200x1600_1200x1600-38bda67bb1290f58e8a18ddc38a3c0ec?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+
+    //
+
+
+    { name: 'EA SPORTS FC™ 24 Standard Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24StandardEdition_EACanada_S2_1200x1600-5ecbb77fd9c0601ef34c4a988120fc42?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Red Dead Redemtion 2',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/epic/offer/RDR2PC1227_Epic Games_860x1148-860x1148-b4c2210ee0c3c3b843a8de399bfe7f5c.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Grand Theft Auto V: Premium Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_1200x1600_Portrait Store Banner-1200x1600-382243057711adf80322ed2aeea42191.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: 'Hogwars Legacy',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S2_1200x1600-2bb94423bf1c7e2fca10577d9f2878b9?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    { name: "Assasin's Creed  Mirage",
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/9bcf5a4dc1d54cb6ab1a42f3a70c5cf4/Carousel_BoxArt_1200x1600_1200x1600-38bda67bb1290f58e8a18ddc38a3c0ec?h=480&quality=medium&resize=1&w=360',
+      category: 'JUEGO BASE',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+
+    //
+
+
     { name: 'EA SPORTS FC™ 24 Standard Edition',
       description: 'Descripción del Producto 1',
       image: 'https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24StandardEdition_EACanada_S2_1200x1600-5ecbb77fd9c0601ef34c4a988120fc42?h=480&quality=medium&resize=1&w=360',
@@ -80,6 +235,8 @@ export class ProductoCarouselComponent {
 
   currentIndex = 0;
   translateX = 0;
+  carouselWidth = 0;
+
 
   prev() {
     if (this.currentIndex > 0) {
@@ -90,17 +247,109 @@ export class ProductoCarouselComponent {
 
   next() {
     if (this.currentIndex < this.products.length - 1) {
+    // if (this.currentIndex < this.products.length) {
       this.currentIndex++;
       this.updateTranslateX();
     }
   }
 
   updateTranslateX() {
-    // this.translateX = -this.currentIndex * 330; // 300 (ancho del item) + 30 (márgenes)
-    // this.translateX = -this.currentIndex * 420; // ... (ancho del item) + 20 (márgenes)
-    this.translateX = -this.currentIndex * 210; // 400 (ancho del item) + 20 (márgenes)
+    // this.translateX = -this.currentIndex * 213; // 400 (ancho del item) + 20 (márgenes)
+    this.translateX = -this.currentIndex * (213 * 5); // 400 (ancho del item) + 20 (márgenes)
   }
 
+  //2do Carousel
+
+
+
+  products_v2 = [
+    {
+      name: 'The Callisto Protocol',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/6b0541b5d9aa476cbf407643ab3b1d7d/EGS_TheCallistoProtocol_StrikingDistanceStudios_S2_1200x1600-1e31eacc92833279f5b7a8d07cd3826c?h=480&quality=medium&resize=1&w=360',
+      category: 'Juego base',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-70 %',
+      originalPrice: '300',
+      discountedPrice: '1200'
+    },
+    {
+      name: 'Fallout: New Vegas',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/3428aaab2c674c98b3acb789dcfaa548/EGS_FalloutNewVegas_ObsidianEntertainment_S2_1200x1600-866fe8b8f56e2e7bb862c49bf0627b9a?h=480&quality=medium&resize=1&w=360',
+      category: 'Juego base',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '44,75',
+      discountedPrice: '179'
+    },
+    {
+      name: 'Shadow of the Tomb Raider: Definitive Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/4b5461ca8d1c488787b5200b420de066/egs-shadowofthetombraiderdefinitiveedition-eidosmontralcrystaldynamicsnixxessoftware-s4-1200x1600-7ee40d6fa744_1200x1600-950cdb624cc75d04fe3c8c0b62ce98de?h=480&quality=medium&resize=1&w=360',
+      category: 'Juego base',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+    {
+      name: 'Mount & Blade II: Bannerlord',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/aeac94c7a11048758064b82f8f8d20ed/EGS_MountBladeIIBannerlord_TaleWorldsEntertainment_S2_1200x1600-67b826955ba37d7d6c33ec578aaa2d54?h=480&quality=medium&resize=1&w=360',
+      category: 'Juego base',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-80 %',
+      originalPrice: '499,99',
+      discountedPrice: '899,99'
+    },
+    {
+      name: 'Gas Station Simulator',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/spt-assets/e48463d2c1fc4e17a3860fbbc8e54edc/gas-station-simulator-6na58.jpg?h=480&quality=medium&resize=1&w=360',
+      category: 'Juego base',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '143,99',
+      discountedPrice: '179,99'
+    },
+    {
+      name: 'Shadow of the Tomb Raider: Definitive Edition',
+      description: 'Descripción del Producto 1',
+      image: 'https://cdn1.epicgames.com/offer/4b5461ca8d1c488787b5200b420de066/egs-shadowofthetombraiderdefinitiveedition-eidosmontralcrystaldynamicsnixxessoftware-s4-1200x1600-7ee40d6fa744_1200x1600-950cdb624cc75d04fe3c8c0b62ce98de?h=480&quality=medium&resize=1&w=360',
+      category: 'Juego base',
+      title: 'Grand Theft Auto V: Premium Edition',
+      discount: '-50 %',
+      originalPrice: '599',
+      discountedPrice: '219.80'
+    },
+
+    // Añadir más productos según sea necesario
+  ];
+
+
+  currentIndex_v2 = 0;
+  translateX_v2 = 0;
+  carouselWidth_v2 = 0;
+
+  prev_v2() {
+    if (this.currentIndex_v2 > 0) {
+      this.currentIndex_v2--;
+      this.updateTranslateX_v2();
+    }
+  }
+
+  next_v2() {
+    if (this.currentIndex_v2 < this.products_v2.length - 1) {
+      this.currentIndex_v2++;
+      this.updateTranslateX_v2();
+    }
+  }
+
+  updateTranslateX_v2() {
+    // this.translateX_v2 = -this.currentIndex_v2 * 213;
+    this.translateX = -this.currentIndex * (213 * 5); // 400 (ancho del item) + 20 (márgenes)
+  }
 
 
 }
